74fbc79683c8e8b2b159172bafee7a0742410fa9
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-12-04 23:29:19 +0600

    problem 6 solved

--- problem_5.dart
+++ problem_5.dart
@@ -1,20 +1,33 @@
 import 'dart:io';
 void main() {
 /*  
-04. Write a Dart program that takes two numbers  and displays the product of two numbers.
+05. Write a Dart program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
 */
-print("Enter first number?");
+print("Enter first number:");
 // Reading number of the first number
 int? firstNumber = int.parse(stdin.readLineSync()!);
 // Printing the firstNumber
 print("First Number is $firstNumber");
-print("Enter Second number?");
+
+print("Enter Second number:");
 // Reading number of the first number
 int? secondNumber = int.parse(stdin.readLineSync()!);
 // Printing the firstNumber
-
 print("Second Number is $secondNumber");
-int productOfTwoNumbers = firstNumber*secondNumber;
 
-print('the product of two numbers is ${productOfTwoNumbers}');
+var productOfTwoNumbers = firstNumber+secondNumber;
+print('the addition of two number\'s result is ${productOfTwoNumbers}');
+
+var multiplyOfTwoNumbers = firstNumber*secondNumber;
+print('the multiply of two number\'s result is ${multiplyOfTwoNumbers}');
+
+var subtractOfTwoNumbers = firstNumber+secondNumber;
+print('the subtract of two number\'s result is ${subtractOfTwoNumbers}');
+
+var divideOfTwoNumbers = firstNumber/secondNumber;
+print('the divide of two number\'s result is ${divideOfTwoNumbers}');
+
+
+var remainderOfTwoNumbers = firstNumber%secondNumber;
+print('the remainder of two number\'s result is ${remainderOfTwoNumbers}');
 }
