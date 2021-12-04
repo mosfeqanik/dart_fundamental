923bbaf41d04ba448a6da819f1c3079fd704b8a7
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-12-04 22:10:06 +0600

    Problem 1 Done

---
+++ Problem_1.dart
@@ -0,0 +1,6 @@
+void main() {
+  
+// 01. Write a Dart program to print 'Hello' on screen and then print your name on a separate line.
+print('Hello');
+print('\nMd Mosfeq Anik');
+}
--- dartfile.dart
+++ dartfile.dart
@@ -1,5 +1,15 @@
 void main() {
   
-
-
+List<String> names =[];
+names.add('anik');
+print(names);
+ var generatedList=['Gias Uddin Samir', 1811913041, ['Phy', 'Che', 'Math'], {'name': 'sazzad', 'phone': 0145445555}];
+generatedList.clear();
+print('generatedlist Result  ${generatedList}');
+generatedList.add('Md Mosfeq Anik');
+generatedList.add('0181818181818');
+generatedList.add('mdkdd@mail.com');
+generatedList.add(['cse','Flutter','dart']);
+generatedList.add({'address':'Mirpur'});
+print('generatedlist Result  ${generatedList}');
 }
