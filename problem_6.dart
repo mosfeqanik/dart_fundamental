30d4a84b0149a78b373fd9376f72957aec52a7a2
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-12-05 10:08:13 +0600

    problem 6 solved

--- problem_6.dart
+++ problem_6.dart
@@ -4,15 +4,16 @@ void main() {
 06. Write a Dart program to print the area (A=πr2) and perimeter of a circle (​​C=2πr) here π=3.1416
 */
 print("Enter Radius:");
-// Reading Radius of the first number
+// Reading Radius
 int? radius = int.parse(stdin.readLineSync()!);
-// Printing the firstNumber
 print("the area  is $radius");
 double pie = 3.1416;
 var areaOfCircle = pie*(radius*radius);
+// Printing the area
 print('The Area of Circle is ${areaOfCircle}');
-
+// Printing the Perimeter
 var perimeterOfTheCircle = 2*pie*(radius*radius);
 print('The Perimeter of Circle is ${perimeterOfTheCircle}');
+
 
 }
