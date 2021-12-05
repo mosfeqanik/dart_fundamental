fbc0480f5085c3aa7d56978faaf2e7555bc584f0
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-12-05 10:28:32 +0600

    problem 7 solved

--- problem_7.dart
+++ problem_7.dart
@@ -4,18 +4,13 @@ void main() {
 07. Make a reusable method  and pass two values inside the method, calculate division of two values,
  get the return value and store it on a new variable inside the main method and print the new variable. 
 */
-print("Enter Radius:");
-// Reading Radius 
-int? radius = int.parse(stdin.readLineSync()!);
-
-print("the area  is $radius");
-double pie = 3.1416;
-var areaOfCircle = pie*(radius*radius);
-// Printing the area
-print('The Area of Circle is ${areaOfCircle}');
-// Printing the Perimeter
-var perimeterOfTheCircle = 2*pie*(radius*radius);
-print('The Perimeter of Circle is ${perimeterOfTheCircle}');
-
+print("Enter FirstNumber:");
+int? firstNumber = int.parse(stdin.readLineSync()!);
+print("Enter SecondNumber:");
+int? secondNumber = int.parse(stdin.readLineSync()!);
+divisionOfTwoFunction(firstNumber,secondNumber);
 }
-
+void divisionOfTwoFunction(int firstNumber,int secondNumber){
+var divideOfTwoNumbers = firstNumber/secondNumber;
+print('the divide of two number\'s result is ${divideOfTwoNumbers}');
+}
