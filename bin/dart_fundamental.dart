6d9fc845fb6199b2bf92311cb0c8f2ad7e4ac708
mosfeqanik <mosfeqanik01@gmail.com>
2026-05-13 23:07:28 +0600

    first commit

---
+++ bin/dart_fundamental.dart
@@ -0,0 +1,42 @@
+import 'dart:ffi';
+
+void main() {
+  int age = 10;
+  double height = 1.75;
+  String name = "";
+  String? nickname = null;
+  print("Nickname: ${nickname ?? "Taj"}");
+  print("");
+  String descripttion = """
+mroshed is a software engineer with expertise in Dart programming. He has a passion for developing efficient and scalable applications. With a strong background in computer science, Morshed is dedicated to creating innovative solutions and contributing to the tech community.
+""";
+  bool isStudent = true;
+
+  var path = r"C:\Users\morshed\Desktop\dart_fundamental.dart";
+  '';
+  print(
+    "Name: $name, Age: $age, Height: $height, Is Student: $isStudent his description is $descripttion $path",
+  );
+  var temperature = 20;
+  if (temperature > 25) {
+    print("It's a hot day");
+  } else {
+    print("It's a pleasant day");
+  }
+  var day = "Friday";
+  switch (day) {
+    case "Monday":
+      print("Its a monday");
+      break;
+    case "Sunday":
+      print("Its a sunday");
+    case "Tuesday":
+      print("Its a Tuesday");
+    case "Wednesday":
+      print("Its a wednesday");
+    case "Thursday":
+      print("Its a thursday");
+    default:
+      print("Its a weekend");
+  }
+}
---
+++ bin/studentcligrading.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+void main() {
+  const String instituteName = "Ostad";
+  final DateTime now = DateTime.now();
+  print("""===================
+  Institute Name: $instituteName
+  Date: ${now}
+  =============================
+  """);
+
+  var grade;
+  int marks;
+  String? name;
+  stdout.write("Enter your name: ");
+  name = name?.trim() ?? "Name not provided";
+  name = stdin.readLineSync()!;
+  print(name);
+  stdout.write("Enter your marks: ");
+  marks = int.parse(stdin.readLineSync()!);
+  print("Your marks: $marks");
+
+  if (marks >= 80) {
+    grade = "A+";
+  } else if (marks >= 70) {
+    grade = "A";
+  } else if (marks >= 60) {
+    grade = "A-";
+  } else if (marks >= 50) {
+    grade = "B";
+  } else {
+    grade = "F";
+  }
+
+  print("Your grade: $grade");
+}
